c6e3d79233f1b1ba13cc000f0d18660f86c90ba5
Thierno88 <82781367+Thierno88@users.noreply.github.com>
2024-03-17 19:14:34 -0300

    First all functional commit game_of_life.cpp

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -1,11 +1,14 @@
+// criar bool de controle para loop start
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <chrono> // Para chrono literals
+#include <thread> // Para std::this_thread::sleep_for
 
 std::vector<std::vector<sf::RectangleShape>> createGrid(int numRows, int numCols, int cellSize, int spacing);
 void paintGrid(sf::RenderWindow& window, std::vector<std::vector<sf::RectangleShape>>grid, int numRows, int numCols);
-void paintClick(sf::RenderWindow& window, std::vector<std::vector<sf::RectangleShape>>& grid, int numRows, int numCols, int cellSize, int spacing);
+void paintClick(sf::RenderWindow& window, std::vector<std::vector<sf::RectangleShape>>& grid, std::vector<std::pair<int, int>>& activeCells, int numRows, int numCols, int cellSize, int spacing);
 void drawMenu(sf::RenderWindow& menu);
-void startGame(sf::RenderWindow& window, sf::RenderWindow& menu);
+void startGame(sf::RenderWindow& menu, std::vector<std::pair<int, int>>& activeCells, std::vector<std::vector<sf::RectangleShape>>& grid, int numRows, int numCols);
 
 int main() {
 
@@ -28,6 +31,9 @@ int main() {
 	sf::RenderWindow menu(sf::VideoMode(width2, height2), "Menu");
 	drawMenu(menu);
 
+	std::vector<std::pair<int, int>> activeCells;
+	
+
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
@@ -37,9 +43,9 @@ int main() {
 
 		window.clear();
 
-		paintGrid(window, grid, numRows, numCols);						//Paint Grid - all cells
-		paintClick(window, grid, numRows, numCols, cellSize, spacing);	//Get mouse click and paint the right cell
-		startGame(window, menu);
+		paintGrid(window, grid, numRows, numCols);                                 // Paint all cells
+		paintClick(window, grid, activeCells, numRows, numCols, cellSize, spacing); // Get mouse click and paint the right cell
+		startGame(menu, activeCells, grid, numRows, numCols);                          // Start the game and update the grid
 
 		window.display();
 	}
@@ -76,7 +82,7 @@ void paintGrid(sf::RenderWindow& window, std::vector<std::vector<sf::RectangleSh
 	}
 }
 
-void paintClick(sf::RenderWindow& window, std::vector<std::vector<sf::RectangleShape>>& grid, int numRows, int numCols, int cellSize, int spacing) {
+void paintClick(sf::RenderWindow& window, std::vector<std::vector<sf::RectangleShape>>& grid, std::vector<std::pair<int, int>>& activeCells, int numRows, int numCols, int cellSize, int spacing) {
 	static bool mouseClicked = false;
 
 	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && !mouseClicked) {
@@ -87,9 +93,12 @@ void paintClick(sf::RenderWindow& window, std::vector<std::vector<sf::RectangleS
 		if (row >= 0 && row < numRows && col >= 0 && col < numCols) {
 			if (grid[row][col].getFillColor() != sf::Color::White) {
 				grid[row][col].setFillColor(sf::Color::White);
+				activeCells.push_back(std::make_pair(row, col));
+
 			}
 			else {
 				grid[row][col].setFillColor(sf::Color(128, 128, 128));
+				activeCells.erase(std::remove(activeCells.begin(), activeCells.end(), std::make_pair(row, col)), activeCells.end());
 			}
 		}
 		mouseClicked = true;
@@ -97,6 +106,8 @@ void paintClick(sf::RenderWindow& window, std::vector<std::vector<sf::RectangleS
 	else if (!sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
 		mouseClicked = false;
 	}
+	
+	return;
 }
 
 void drawMenu(sf::RenderWindow& menu) {
@@ -113,18 +124,59 @@ void drawMenu(sf::RenderWindow& menu) {
 	menu.display();
 }
 
-void startGame(sf::RenderWindow& window, sf::RenderWindow& menu) {
+void startGame(sf::RenderWindow& menu, std::vector<std::pair<int, int>>& activeCells, std::vector<std::vector<sf::RectangleShape>>& grid, int numRows, int numCols) {
 	static bool mouseClicked = false;
+	static bool runIterations = false;
+
 	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && !mouseClicked) {
 		sf::Vector2i mousePosition = sf::Mouse::getPosition(menu);
-		// Verifica se o clique ocorreu dentro das coordenadas do botão
 		if (mousePosition.x >= 50 && mousePosition.x <= 150 &&
 			mousePosition.y >= 25 && mousePosition.y <= 75) {
-			std::cout << "Botão clicado!\n";
+			runIterations = !runIterations;
 		}
 		mouseClicked = true;
 	}
 	else if (!sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
 		mouseClicked = false;
+	}
+
+	if (runIterations) {
+		std::vector<std::vector<sf::RectangleShape>> newGrid = grid; // new grid
+
+		for (int i = 0; i < numRows; i++) {
+			for (int j = 0; j < numCols; j++) {
+				int neighbors = 0;
+
+				// check living neighbors
+				for (int dx = -1; dx <= 1; ++dx) {
+					for (int dy = -1; dy <= 1; ++dy) {
+						if (dx == 0 && dy == 0) continue; // Ignores actual cell
+						int nx = i + dx;
+						int ny = j + dy;
+						if (nx >= 0 && nx < numRows && ny >= 0 && ny < numCols && grid[nx][ny].getFillColor() == sf::Color::White) {
+							neighbors++;
+						}
+					}
+				}
+
+				// Atualiza o estado da célula com base nos vizinhos vivos
+				if (grid[i][j].getFillColor() == sf::Color::White) {
+					if (neighbors < 2 || neighbors > 3) {
+						newGrid[i][j].setFillColor(sf::Color(128, 128, 128)); // A célula morre
+					}
+				}
+				else {
+					if (neighbors == 3) {
+						newGrid[i][j].setFillColor(sf::Color::White); // A célula nasce
+					}
+				}
+			}
+		}
+		grid = newGrid;
+		std::this_thread::sleep_for(std::chrono::milliseconds(400));
+
+		// Atualiza o grid com o novo estado das células
+		//paintGrid(menu, grid, numRows, numCols);
+		//menu.display();
 	}
 }
